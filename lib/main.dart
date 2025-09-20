2228a34d486e3be1503bb760824b89636cea5a38
MasonMathias <mason.mathias04@gmail.com>
2025-09-19 22:35:59 -0400

    implemented the animated image switching

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,13 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   ThemeMode _themeMode = ThemeMode.system;
   int _counter = 0;
+  int _imageIndex = 1;
+  final List<String> _images = const [
+    'https://images.rawpixel.com/image_png_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvam9iNjgwLTE2Ni1wLWwxZGJ1cTN2LnBuZw.png',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQn8XeZpoD0ScPkpnB0WFMtlbu6KevRzmizJw&s',
+  ];
+
+  void _toggleImage() => setState(() => _imageIndex = 1 - _imageIndex);
 
   void _toggleTheme() {
     setState(() {
@@ -56,16 +63,39 @@ class _MyAppState extends State<MyApp> {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
+
                   Text(
                     'You have pushed the button this many times:',
                     style: Theme.of(context).textTheme.bodyLarge,
                     textAlign: TextAlign.center,
                   ),
                   const SizedBox(height: 8),
+
                   Text(
                     '$_counter',
                     style: Theme.of(context).textTheme.headlineMedium,
                   ),
+                  const SizedBox(height: 8),
+                  
+                  ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: SizedBox(
+                    height: 160,
+                    width: 280,
+                    child: AnimatedSwitcher(
+                      duration: const Duration(milliseconds: 350),
+                      switchInCurve: Curves.easeIn,
+                      switchOutCurve: Curves.easeOut,
+                      transitionBuilder: (child, animation) =>
+                          FadeTransition(opacity: animation, child: child),
+                      child: Image.network(
+                        _images[_imageIndex],
+                        key: ValueKey(_images[_imageIndex]),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ),
                 ],
               ),
             ),
@@ -96,7 +126,7 @@ class _MyAppState extends State<MyApp> {
 
                 // Image toggle button
                 FloatingActionButton(
-                  onPressed: () => print('toggled image'),
+                  onPressed: _toggleImage,
                   tooltip: 'Toggle image',
                   child: const Icon(Icons.image),
                 ),
